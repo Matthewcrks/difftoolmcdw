b30a06e4262d1e8a161aaac0564f1f4df121b59b
Daniel Wang <itsdwang@gmail.com>
2018-01-20 18:18:00 -0800

    Added second text file to ArrayList

--- DiffTool.java
+++ DiffTool.java
@@ -1,8 +1,12 @@
+import java.util.*;
+
 public class DiffTool {
 	public SinglyLinkedList ll;
+	public ArrayList<String> secondText;
 
 	public DiffTool() {
 		ll = new SinglyLinkedList();
+		secondText = new ArrayList<String>();
 	}
 
 	public static void main(String[] args) {
@@ -12,7 +16,27 @@ public class DiffTool {
 
 	public void run() {
 		String testString = "the quick brown fox";
+		String testString2 = "fox and quick brown";
+		
 		parseString(testString);
+
+		int stringCode = 0;
+		ListNode temp = ll.first;
+
+		while (temp != null) {
+			System.out.println("hash code for " + ((Word)(temp.data)).word + " is " + ((Word)(temp.data)).word.hashCode());
+			stringCode += ((Word)(temp.data)).word.hashCode();
+
+			temp = temp.next;
+		}
+
+		// looking at second text file now
+		parseSecondFile(testString2);
+		// file added to linked list now
+
+		for (String t: secondText) {
+			System.out.println(t);
+		}
 	}	
 
 	public void parseString(String s) {
@@ -27,5 +51,18 @@ public class DiffTool {
 		}
 
 		ll.printList();
+	}
+
+	public void parseSecondFile(String s) {
+		// String[] splitString = s.split("[\\p{Punct}]+");
+		String[] splitString = s.split("\\.");
+
+		for(String p: splitString) {
+			secondText.add(p);
+		}
+	}
+
+	public int generateHashCode(String s) {
+		return s.hashCode();
 	}
 }
